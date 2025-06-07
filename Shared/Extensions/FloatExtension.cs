01504fbd51012e8d418eded87fd50f2eca0d4a26
Zonakkis <2720716602@qq.com>
2025-06-08 00:47:42 +0800

    更新命名空间引用
    在 `FloatExtension.cs` 文件中，移除了对 `Shared.Models.Entities` 的引用，并添加了对 `Shared.Models.VOs` 的引用。此更改旨在更新命名空间的使用，以更好地符合项目结构。`FromMilliseconds` 方法的实现保持不变，继续将毫秒转换为 `SpeedrunTime` 对象。

--- Shared/Extensions/FloatExtension.cs
+++ Shared/Extensions/FloatExtension.cs
@@ -1,5 +1,4 @@
-﻿using Shared.Models.Entities;
-using Shared.Models.VOs;
+﻿using Shared.Models.VOs;
 
 namespace Shared.Extensions
 {
@@ -7,7 +6,6 @@ namespace Shared.Extensions
     {
         public static SpeedrunTime FromMilliseconds(this float milliseconds)
         {
-            var time = TimeSpan.FromMilliseconds(milliseconds);
             return new SpeedrunTime(0, 0, (int)(milliseconds * 1000));
         }
     }
