3ece35570ec9b508fbf8623b11e40cc2f2237cf1
Zonakkis <2720716602@qq.com>
2025-06-08 00:51:15 +0800

    优化代码结构和清理不必要的文件
    更新 `.gitignore` 文件以包含 Azure Function 相关的文件和目录，移除了不再需要的注释和文件路径。删除了 `LeanCloudConstants.cs` 和 `SQLConstants.cs` 文件中的命名空间和类定义，简化了代码结构。移除了多个文件中的不必要的 `using` 语句和类定义，简化了数据库上下文、仓库和服务的实现。修正了 `ILeanCloudRepository.cs` 中的命名空间拼写错误。最后，清理了 `host.json` 中的不必要注释，优化了 Azure Function 的配置文件。

--- Backend.AzureFunction/Constants/LeanCloudConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Backend.Constants
-{
-    public static class LeanCloudConstants
-    {
-        public const string HttpClientName = "LeanCloud";
-        public const string RequestPrefix = "/1.1/classes";
-    }
-}
--- Backend.AzureFunction/Repositories/Interfaces/ILeanCloudRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Backend.Reposties.Interfaces
-{
-    internal interface ILeanCloudRepository<T>
-    {
-        Task<T?> GetAsync(string id);
-        Task<List<T>> ListAsync();
-    }
-}
